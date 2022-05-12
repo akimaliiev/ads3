a4a0658da8693dd37a2eea34f779845cb57b564d
akimaliiev <103598323+akimaliiev@users.noreply.github.com>
2022-05-12 13:57:06 +0600

    Update BST.java

--- BST.java
+++ BST.java
@@ -1,15 +1,8 @@
 package com.company;
 
-import org.w3c.dom.Node;
-
-import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.function.Consumer;
-
 public class BST <K extends Comparable<K>,V> {
-    
 
-    //node class that defines BST node
+
     private class Node {
         private K key;
         private V val;
@@ -25,37 +18,44 @@ public class BST <K extends Comparable<K>,V> {
     // BST root node
     private Node root;
 
-    // Constructor for BST =>initial empty tree
     public BST() {
         root = null;
     }
-
-    //delete a node from BST
-    public void deleteKey(K key) {
-        delete(root, key);
+    public BST(K key, V val){
+        root=new Node(key,val);
     }
 
-    //recursive delete function
-    private Node delete(Node root, K key) {
-        //base case
-        if (root == null) return root;
+    // putting
+    public void insert(K key, V val) {
+        root = insert(root, key, val);
+    }
 
-        if (key.compareTo(root.key) < 0)     //traverse left subtree
-            root.left = delete(root.left, key);
-        else if (0 < key.compareTo(root.key))  //traverse right subtree
-            root.right = delete(root.right, key);
-        else {
-            // node contains only one child
-            if (root.left == null)
-                return root.right;
-            else if (root.right == null)
-                return root.left;
-
-            root.key = minValue(root.right);
-
-            root.right = delete(root.right, root.key);
+    // insert function
+    private Node insert(Node root, K key, V val) {
+        if (root == null) {
+            root = new Node(key, val);
+            return root;
         }
+        if (key.compareTo(root.key) < 0)    
+            root.left = insert(root.left, key, val);
+        else if (0 < key.compareTo(root.key))    
+            root.right = insert(root.right, key, val);
         return root;
+    }
+
+    public V get(K key){
+        return get(root,key);
+
+    }
+    private V get(Node root, K key){
+        if(root==null)
+            return null;
+        else if(root.key.compareTo(key)==0)
+            return root.val;
+        else if(root.key.compareTo(key)<0)
+            return get(root.right,key);
+        else
+            return get(root.left,key);
     }
 
     private K minValue(Node root) {
@@ -68,68 +68,35 @@ public class BST <K extends Comparable<K>,V> {
         return minval;
     }
 
-    // insert a node in BST 
-    public void insert(K key, V val) {
-        root = insert(root, key, val);
+    public void delete(K key) {
+        delete(root, key);
     }
 
-    //recursive insert function
-    private Node insert(Node root, K key, V val) {
-        if (root == null) {
-            root = new Node(key, val);
-            return root;
+    // delete function
+    private Node delete(Node root, K key) {
+        //base case
+        if (root == null) return null;
+
+        if (key.compareTo(root.key) < 0)     
+            root.left = delete(root.left, key);
+        else if (0 < key.compareTo(root.key)) 
+            root.right = delete(root.right, key);
+        else {
+            if (root.left == null)
+                return root.right;
+            else if (root.right == null)
+                return root.left;
+
+            root.key = minValue(root.right);
+
+            root.right = delete(root.right, root.key);
         }
-        if (key.compareTo(root.key) < 0)     //insert in the left subtree
-            root.left = insert(root.left, key, val);
-        else if (0 < key.compareTo(root.key))    //insert in the right subtree
-            root.right = insert(root.right, key, val);
-        // return pointer
         return root;
     }
 
-    // method for inorder traversal of BST
-    public void inorder() {
-        inorder(root);
+    public Iterable<K> Iterator(){
+        return (Iterable<K>)root.key;
     }
 
-    // recursively traverse the BST  
-    private void inorder(Node root) {
-        if (root != null) {
-            inorder(root.left);
-            System.out.print(root.key + "=>" + root.val);
-            inorder(root.right);
-        }
-    }
 
-    public void postOrder() {
-        postOrder(root);
-    }
-
-    private void postOrder(Node root) {
-        if (root != null) {
-            postOrder(root.right);
-            System.out.println(root.key + "=>" + root.val);
-            postOrder(root.left);
-        }
-    }
-
-    public boolean search(K key) {
-        Node node = root;
-        return search(node, key);
-    }
-
-    private boolean search(Node node, K key) {
-        if (node == null) {
-            return false;
-        }
-        if (node.key == key) {
-            return true;
-        } else {
-            if (node.key.compareTo(key) > 0) {
-                return search(node.right, key);
-            } else {
-                return search(node.left, key);
-            }
-        }
-    }
 }
